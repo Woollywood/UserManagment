4733b341c0758a80fcaf25f49bc5377bae9657fb
Woollywood <115595112+Woollywood@users.noreply.github.com>
2024-08-14 22:54:09 +0300

    refactor

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -1,6 +1,4 @@
 import { Outlet } from 'react-router-dom';
-import { Link } from '@nextui-org/link';
-
 import { Navbar } from '@/components/navbar';
 
 export default function DefaultLayout() {
@@ -10,16 +8,6 @@ export default function DefaultLayout() {
 			<main className='container mx-auto max-w-7xl flex-grow px-6 pt-16'>
 				<Outlet />
 			</main>
-			<footer className='flex w-full items-center justify-center py-3'>
-				<Link
-					isExternal
-					className='flex items-center gap-1 text-current'
-					href='https://nextui-docs-v2.vercel.app?utm_source=next-pages-template'
-					title='nextui.org homepage'>
-					<span className='text-default-600'>Powered by</span>
-					<p className='text-primary'>NextUI</p>
-				</Link>
-			</footer>
 		</div>
 	);
 }
