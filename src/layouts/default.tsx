1973e829a6e2303aa900db2b6a81e6aad3b6146e
Woollywood <115595112+Woollywood@users.noreply.github.com>
2024-08-14 21:30:01 +0300

    prettier

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -1,29 +1,22 @@
-import { Link } from "@nextui-org/link";
+import { Link } from '@nextui-org/link';
 
-import { Navbar } from "@/components/navbar";
+import { Navbar } from '@/components/navbar';
 
-export default function DefaultLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <div className="relative flex flex-col h-screen">
-      <Navbar />
-      <main className="container mx-auto max-w-7xl px-6 flex-grow pt-16">
-        {children}
-      </main>
-      <footer className="w-full flex items-center justify-center py-3">
-        <Link
-          isExternal
-          className="flex items-center gap-1 text-current"
-          href="https://nextui-docs-v2.vercel.app?utm_source=next-pages-template"
-          title="nextui.org homepage"
-        >
-          <span className="text-default-600">Powered by</span>
-          <p className="text-primary">NextUI</p>
-        </Link>
-      </footer>
-    </div>
-  );
+export default function DefaultLayout({ children }: { children: React.ReactNode }) {
+	return (
+		<div className='relative flex h-screen flex-col'>
+			<Navbar />
+			<main className='container mx-auto max-w-7xl flex-grow px-6 pt-16'>{children}</main>
+			<footer className='flex w-full items-center justify-center py-3'>
+				<Link
+					isExternal
+					className='flex items-center gap-1 text-current'
+					href='https://nextui-docs-v2.vercel.app?utm_source=next-pages-template'
+					title='nextui.org homepage'>
+					<span className='text-default-600'>Powered by</span>
+					<p className='text-primary'>NextUI</p>
+				</Link>
+			</footer>
+		</div>
+	);
 }
